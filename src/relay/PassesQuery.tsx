01cd4e42a4288eab6d46ae563c8755ba01037114
Larissa Thaís de Farias <larissathasdefar@hotmail.com>
2019-09-18 15:32:00 -0300

    fetching passes using simple architecture

--- src/relay/PassesQuery.tsx
+++ src/relay/PassesQuery.tsx
@@ -1,46 +1,37 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-import {graphql, createFragmentContainer} from 'react-relay';
-import createQueryRenderer from './QueryRender';
+import {graphql, QueryRenderer} from 'react-relay';
 import environment from './Environment'
 
-const Passes = (props) => (
-  <View>
-    {console.log(props)}
-    <Text>aaa</Text>
-  </View>
-);
-
-const PassesFragmentContainer = createFragmentContainer(
-  Passes, {
-  passes: graphql`
-    fragment PassesQuery_passes on Query {
-      passes {
-        edges {
-          node {
-            id
-            website
-            login
-            password
+const Passes = () => (
+  <QueryRenderer
+    environment={environment}
+    query={graphql`
+      query PassesQuery {
+        passes {
+          edges {
+            node {
+              id
+              website
+              login
+              password
+            }
           }
         }
       }
-    }
-  `,
-});
-
-const PassesQuery = createQueryRenderer(
-  PassesFragmentContainer,
-  Passes,
-  {
-    query: graphql`
-      query PassesQuery {
-        ...PassesQuery_passes
+    `}
+    variables={{}}
+    render={({error, props}) => {
+      console.log(props)
+      if (error) {
+        return <Text>Error!</Text>;
       }
-    `,
-    getFragmentProps: ({ passes }) => ({passes}),
-  },
-);
+      if (!props) {
+        return <Text>Loading...</Text>;
+      }
+      return <Text>Diz que vai filhoo</Text>;
+    }}
+  />
+)
 
-export default PassesQuery;
-
+export default Passes;
--- src/relay/__generated__/PassesQuery.graphql.ts
+++ src/relay/__generated__/PassesQuery.graphql.ts
@@ -1,10 +1,18 @@
 /* tslint:disable */
 
 import { ConcreteRequest } from "relay-runtime";
-import { PassesQuery_passes$ref } from "./PassesQuery_passes.graphql";
 export type PassesQueryVariables = {};
 export type PassesQueryResponse = {
-    readonly " $fragmentRefs": PassesQuery_passes$ref;
+    readonly passes: {
+        readonly edges: ReadonlyArray<{
+            readonly node: {
+                readonly id: string;
+                readonly website: string | null;
+                readonly login: string | null;
+                readonly password: string | null;
+            };
+        } | null>;
+    } | null;
 };
 export type PassesQuery = {
     readonly response: PassesQueryResponse;
@@ -15,10 +23,6 @@ export type PassesQuery = {
 
 /*
 query PassesQuery {
-  ...PassesQuery_passes
-}
-
-fragment PassesQuery_passes on Query {
   passes {
     edges {
       node {
@@ -32,7 +36,71 @@ fragment PassesQuery_passes on Query {
 }
 */
 
-const node: ConcreteRequest = {
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "kind": "LinkedField",
+    "alias": null,
+    "name": "passes",
+    "storageKey": null,
+    "args": null,
+    "concreteType": "PassConnection",
+    "plural": false,
+    "selections": [
+      {
+        "kind": "LinkedField",
+        "alias": null,
+        "name": "edges",
+        "storageKey": null,
+        "args": null,
+        "concreteType": "PassEdge",
+        "plural": true,
+        "selections": [
+          {
+            "kind": "LinkedField",
+            "alias": null,
+            "name": "node",
+            "storageKey": null,
+            "args": null,
+            "concreteType": "Pass",
+            "plural": false,
+            "selections": [
+              {
+                "kind": "ScalarField",
+                "alias": null,
+                "name": "id",
+                "args": null,
+                "storageKey": null
+              },
+              {
+                "kind": "ScalarField",
+                "alias": null,
+                "name": "website",
+                "args": null,
+                "storageKey": null
+              },
+              {
+                "kind": "ScalarField",
+                "alias": null,
+                "name": "login",
+                "args": null,
+                "storageKey": null
+              },
+              {
+                "kind": "ScalarField",
+                "alias": null,
+                "name": "password",
+                "args": null,
+                "storageKey": null
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+];
+return {
   "kind": "Request",
   "fragment": {
     "kind": "Fragment",
@@ -40,89 +108,22 @@ const node: ConcreteRequest = {
     "type": "Query",
     "metadata": null,
     "argumentDefinitions": [],
-    "selections": [
-      {
-        "kind": "FragmentSpread",
-        "name": "PassesQuery_passes",
-        "args": null
-      }
-    ]
+    "selections": (v0/*: any*/)
   },
   "operation": {
     "kind": "Operation",
     "name": "PassesQuery",
     "argumentDefinitions": [],
-    "selections": [
-      {
-        "kind": "LinkedField",
-        "alias": null,
-        "name": "passes",
-        "storageKey": null,
-        "args": null,
-        "concreteType": "PassConnection",
-        "plural": false,
-        "selections": [
-          {
-            "kind": "LinkedField",
-            "alias": null,
-            "name": "edges",
-            "storageKey": null,
-            "args": null,
-            "concreteType": "PassEdge",
-            "plural": true,
-            "selections": [
-              {
-                "kind": "LinkedField",
-                "alias": null,
-                "name": "node",
-                "storageKey": null,
-                "args": null,
-                "concreteType": "Pass",
-                "plural": false,
-                "selections": [
-                  {
-                    "kind": "ScalarField",
-                    "alias": null,
-                    "name": "id",
-                    "args": null,
-                    "storageKey": null
-                  },
-                  {
-                    "kind": "ScalarField",
-                    "alias": null,
-                    "name": "website",
-                    "args": null,
-                    "storageKey": null
-                  },
-                  {
-                    "kind": "ScalarField",
-                    "alias": null,
-                    "name": "login",
-                    "args": null,
-                    "storageKey": null
-                  },
-                  {
-                    "kind": "ScalarField",
-                    "alias": null,
-                    "name": "password",
-                    "args": null,
-                    "storageKey": null
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      }
-    ]
+    "selections": (v0/*: any*/)
   },
   "params": {
     "operationKind": "query",
     "name": "PassesQuery",
     "id": null,
-    "text": "query PassesQuery {\n  ...PassesQuery_passes\n}\n\nfragment PassesQuery_passes on Query {\n  passes {\n    edges {\n      node {\n        id\n        website\n        login\n        password\n      }\n    }\n  }\n}\n",
+    "text": "query PassesQuery {\n  passes {\n    edges {\n      node {\n        id\n        website\n        login\n        password\n      }\n    }\n  }\n}\n",
     "metadata": {}
   }
 };
-(node as any).hash = '2692659233cc2a7858d2cd11a124dae8';
+})();
+(node as any).hash = 'b748e9e83bd12a1c72ad6f4753a9b862';
 export default node;
